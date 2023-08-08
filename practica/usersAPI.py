45aad1e4cd79e4433613993e3d2734f2dd070161
JuanCaceffo <caceffojuan@gmail.com>
2023-08-08 14:05:08 -0300

    refactor de manejo de errores, aplicacione de codigos de estados http

--- practica/usersAPI.py
+++ practica/usersAPI.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,102 +15,95 @@ users_list = [Users(id= 1,name="juanchi",lastname="caceffo", age=20),
              Users(id= 2,name="facundo",lastname="barneche",age=30),
              Users(id= 3,name="alejo",  lastname="menini",  age=22)]
 ##--------------------------------------- base de datos imaginara ---------------------------------------
-RAISE_ERROR_USERNOTFOUND = {"error": "user not found"}
-RAISE_ERROR_USEREXIST = {"error": "User exist"}
-RAISE_ERROR_USERDONOTEXIST = {"error": "User doesn't exist"}
+
+##--------------------------------------- mensjaes error ---------------------------------------
+ERROR_USERNOTFOUND = "user not found"
+ERROR_USEREXIST = "User exist"
+ERROR_USERDONOTEXIST = "User doesn't exist"
+##--------------------------------------- mensjaes error ---------------------------------------
+
+##--------------------------------------- Validcaiones ---------------------------------------
+def errorUserNotFund(users:list):
+    if (len(users) == 0): raise HTTPException(status_code=404,detail=ERROR_USERNOTFOUND)
+def errorUserExist(id:int):
+    users = list(filter(lambda user: user.id == id,users_list))
+    if (len(users) != 0): raise HTTPException(status_code=409,detail=ERROR_USEREXIST)
+##--------------------------------------- Validcaiones ---------------------------------------
+
 ##-------------------------------------------------- GET -------------------------------------------------- 
+
 @app.get("/users")
 async def getUsers():
     return users_list
 
-def search_user(id: int):
-    users = filter(lambda user: user.id == id, users_list)
-    try:
-        return list(users)[0]
-    except:
-        return RAISE_ERROR_USERNOTFOUND
-
-
 #para parametrizar un pedido a la api en el path ponemos el parametro que vamos a recibir entre corchetes
 #dentro del get
-@app.get("/user/{id}")
+@app.get("/user/{id}",status_code=200)
 async def getUserByID(id: int):
-    return search_user(id)
+    users = list(filter(lambda user: user.id == id, users_list))
+    errorUserNotFund(users)
+    return list(users)[0]
 
 
 ##busqueda por id utilizando query en vez de path
 ##la llamada a este get se hace de la siguiente forma
 ##http://127.0.0.1:8000/userquery/?parametro=dato
-@app.get("/userquery/")
+@app.get("/userquery/",status_code=200)
 async def userQueryId(id: int):
-    return search_user(id)
+    users = list(filter(lambda user: user.id == id, users_list))
+    errorUserNotFund(users)
+    return list(users)[0]
 
-@app.get("/userQueryNameLastname/")
+
+@app.get("/userQueryNameLastname/",status_code=200)
 async def userQueryNameLastname(name: str,lastName: str):
     """Busqueda por nombre y apellido"""
     name = name.lower( )
     lastName = lastName.lower()
     users = [user for user in users_list if (user.name == name and user.lastname == lastName)]
-    try:
-        return users[0]
-    except:
-        return RAISE_ERROR_USERNOTFOUND
+    errorUserNotFund(users)
+    return users
 
 ##IMPORTANTE EL PATH SE UTILIZA PAR APARAMETROS QUE VAN FIJOS Y LA QUERY PARA LOS OPCIONALES, (POR CONVENCION)  
 ##-------------------------------------------------- GET -------------------------------------------------- 
 
 ##-------------------------------------------------- POST -------------------------------------------------- 
 
-@app.post("/user/")
+@app.post("/user/",status_code=201)
 async def user(user: Users):
-    try:   
-        errorUserExist(user)
-        users_list.append(user)
-        return user
-    except ValueError as e:
-        return e.__str__()
-
-
-def errorUserExist(user: Users):
-    if (type(search_user(user.id)) == Users):
-        raise ValueError(RAISE_ERROR_USEREXIST)
+    errorUserExist(user.id)
+    users_list.append(user)
         
 ##-------------------------------------------------- POST -------------------------------------------------- 
 
 ##-------------------------------------------------- PUT -------------------------------------------------- 
-@app.put("/user/")
+
+
+@app.put("/user/",status_code=201)
 async def user(user: Users):
-    try:
-        errorUserDosentExist(user)
-        for index, value in enumerate(users_list):
-            if user.id == value.id:
-                users_list[index] = user
-                return user
-    except Exception as e:
-        return e.__str__()
-    
-def errorUserDosentExist(user:Users):
-    if (search_user(user.id) == RAISE_ERROR_USERNOTFOUND):
-        raise Exception(RAISE_ERROR_USERNOTFOUND)
+    try:   
+        index = [user_.id for user_ in users_list].index(user.id)
+        users_list[index] = user
+    except ValueError:    
+        raise HTTPException(status_code=404,detail=ERROR_USERDONOTEXIST)
 ##-------------------------------------------------- PUT -------------------------------------------------- 
 
 ##-------------------------------------------------- DELETE -------------------------------------------------- 
 @app.delete("/user/{id}")
 async def user(id:int):
     try:
-        if (search_user(id) == RAISE_ERROR_USERNOTFOUND):
-            raise Exception(RAISE_ERROR_USERNOTFOUND)
-        for i, user in enumerate(users_list):
-            if user.id == id:
-                return users_list.pop(i)
-    except Exception as e:
-        return str(e)
-    
+        index = [user.id for user in users_list].index(id)
+        users_list.pop(index)
+    except ValueError:
+        raise HTTPException(status_code=404,detail=ERROR_USERDONOTEXIST)
+
 @app.delete("/user/{name}/{lastName}")
 async def user(name:str,lastName:str):
     name = name.lower()
     lastName = lastName.lower()
     global users_list
-    users_list = list(filter(lambda user: (user.name != name and user.lastname != lastName), users_list)) 
+    USERS = list(filter(lambda user: (user.name != name and user.lastname != lastName), users_list))
+    if (len(USERS) == len(users_list)): raise HTTPException(status_code=409,detail=ERROR_USERDONOTEXIST)
+    users_list = USERS 
 
 ##-------------------------------------------------- DELETE -------------------------------------------------- 
